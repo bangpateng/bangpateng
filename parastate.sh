5598e41dd5f62cdf8af94d96055cce10fc02b903
Bang Pateng <38981255+bangpateng@users.noreply.github.com>
2022-08-16 14:33:44 +0700

    Update parastate.sh

--- parastate.sh
+++ parastate.sh
@@ -10,13 +10,6 @@ echo -e "\e[0m"
 echo "=============================================================="
 sleep 2
 
-echo -e '\e[36mAuthor :\e[39m' Bang Pateng
-echo -e '\e[36mTelegram Group :\e[39m' @bangpateng_group
-echo -e '\e[36mTelegram Channel :\e[39m' @bangpateng_airdrop
-echo -e '\e[36mYoutube :\e[39m' Bang Pateng
-echo -e '\e[36mWebsite :\e[39m' www.bangpatengnode.site
-echo "======================================="
-
 echo -e '\e[1;33m1.Update packagese\e[0m'
 sudo apt update && sudo apt upgrade -y
 
